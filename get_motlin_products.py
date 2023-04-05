7015287362178db307ed94456c6a9deb896c3525
Artsiom Babrounik <artsiom.babrounik>
2023-04-05 21:39:08 +0400

    Added code to request products data.

--- get_motlin_products.py
+++ get_motlin_products.py
@@ -2,33 +2,28 @@ import requests
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+from urllib.parse import urljoin
 
 
 def main():
     env_file_path = Path.cwd() / ".env"
     load_dotenv(env_file_path)
-    store_id = os.environ['STORE_ID']
-    client_secret = os.environ['CLIENT_SECRET']
     client_id = os.environ['CLIENT_ID']
-
     api_base_url = 'https://api.moltin.com'
-    products_uri = '/pcm/products'
-
-    auth_url = f'{api_base_url}/oauth/{client_secret}'
-    params = {
-        'client_id': client_id,
-        'grant_type': 'implicit'
-    }
-    response = requests.post(auth_url, data=params)
+    auth_url = urljoin(api_base_url, 'oauth/access_token')
+    print(auth_url)
+    params = {'client_id': client_id, 'grant_type': 'implicit'}
+    response = requests.post(auth_url, data=params)  # , headers=headers)
     response.raise_for_status()
+    response_dict = response.json()
+    access_token = response_dict["access_token"]
 
-    print(response.json())
+    headers = {'Authorization': f'Bearer {access_token}'}
+    products_url = "https://api.moltin.com/pcm/products"
+    products_response = requests.post(products_url, headers=headers)
+    products_response.raise_for_status()
+    print(products_response.json())
 
-
-# curl https://api.moltin.com/v2/carts/abc \
-#      -H "Authorization: Bearer XXXX"
-
-# response = requests.post(url, json=data, headers=headers)
 
 if __name__ == '__main__':
     main()
